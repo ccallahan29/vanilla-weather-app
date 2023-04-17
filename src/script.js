93d8a387a60bcfeee4e838a56baec578d57a89b1
Carina Callahan <carina.callahan29@gmail.com>
2023-04-16 22:09:33 -0400

    Search bar added
    search form added

--- src/script.js
+++ src/script.js
@@ -29,16 +29,32 @@ function showTemp(response) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind-speed");
   let dateElement = document.querySelector("#current-date");
+  let iconElement = document.querySelector("#weather-icon");
   cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = response.data.temperature.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.time * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
+  );
+  iconElement.setAttribute("alt", response.data.condition.description);
 }
 
-let apiKey = "303c103b672ao77tf0b47afa0937b5c6";
-let units = "metric";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Berlin&key=${apiKey}&${units}`;
+function search(city) {
+  let apiKey = "303c103b672ao77tf0b47afa0937b5c6";
+  let units = "metric";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&${units}`;
+  axios.get(apiUrl).then(showTemp);
+}
 
-axios.get(apiUrl).then(showTemp);
+function searchCity(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#input-city");
+  search(cityInputElement.value);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", searchCity);
